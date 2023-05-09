9b6d17b61ba141bd4a9cf1316ac41929525de9a5
lorenzo-molina <116020466+lorenzo-molina@users.noreply.github.com>
2023-05-09 16:20:23 +0200

    Création des vérifications

--- sudoku.py
+++ sudoku.py
@@ -1,2 +1,5 @@
 #  1 : Création de la grille de jeu avec des chiffres placés aléatoirement.
 #  2 : Input pour le joueur afin qu'il puisse placer son chiffre
+#  3 : Vérification si le chiffre est déja dans le bloc, sur la ligne ou sur la colonne
+#  3a : Si au moins un de ces critères est vérifié, alors renvoyer le joueur à l'input en lui indiquant que le placement n'est pas possible sur cette case
+#  3b : Sinon placer le chiffre dans la grille
